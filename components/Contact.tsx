e02c337e8ccbc43cc262d1a0085e538403b9003d
= <cai.keighley@protonmail.com>
2023-07-04 21:05:23 +0100

    remove honeypot, add recaptcha

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -9,12 +9,14 @@ const Contact = () => {
         <FadeIn>
             <div className="py-10 px-6 my-4 sm:px-10 xl:p-12 rounded-lg bg-tertiary/10">
                 <h3 className="text-2xl font-medium text-primary" id="contact-us">Get in touch</h3>
-                <form name="contact" method="POST" action="/success" netlify-honeypot="bot-field" data-netlify="true" className="mt-6 grid grid-cols-1 sm:grid-cols-2 gap-y-2 sm:gap-x-8 sm:gap-y-4">
-                    <p className="hidden">
-                        <label>
-                            Don&apos;t fill this out if you&apos;re human: <input name="bot-field" />
-                        </label>
-                    </p>
+                <form 
+                    name="contact" 
+                    method="POST"
+                    action="/success" 
+                    data-netlify="true"
+                    data-netlify-recaptcha="true"
+                    className="mt-6 grid grid-cols-1 sm:grid-cols-2 gap-y-2 sm:gap-x-8 sm:gap-y-4"
+                >
                     <div className="sm:col-span-2">
                         <label htmlFor="name" className="block text-sm font-medium text-primary">
                             Name
@@ -104,6 +106,7 @@ const Contact = () => {
                             />
                         </div>
                     </div>
+                    <div data-netlify-recaptcha="true"></div>
                     <div className="sm:col-span-2 sm:flex sm:justify-end">
                         <input
                             type="submit"
